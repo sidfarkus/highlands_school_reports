d9185a5f187270d7fed4c9eceb75b147763cfd43
Anna Thomas <anna.thomas67@yahoo.com>
2012-10-20 09:32:51 -0400

    import

---
+++ Highlands/Model/grade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Highlands.Model
+{
+    public class grade
+    {
+        public string StudentKey;
+        public string CourseKey;
+        public string LetterGrade;
+        public string SpecialGrade;
+        public string Comment;
+        public string Approval;
+    }
+}
---
+++ Highlands/Model/selfdevscore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Highlands.Model
+{
+    public class selfdevscore
+    {
+        public string StudentKey = string.Empty;
+        public string Area = string.Empty;
+        public string Quarter = string.Empty;
+        public string Score = string.Empty;
+        public string Teacher = string.Empty;
+    }
+}
