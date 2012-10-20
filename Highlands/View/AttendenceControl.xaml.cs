2eeaf56e7eb539c4a6c75ff5ff6aa0deb0ba83e0
Ron Warholic <blodbath@gmail.com>
2012-10-20 00:01:35 -0400

    Added new controls

--- Highlands/View/AttendenceControl.xaml.cs
+++ Highlands/View/AttendenceControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Highlands.View
+namespace Highlands
 {
     /// <summary>
     /// Interaction logic for AttendeesControl.xaml
