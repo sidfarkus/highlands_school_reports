9566155a5e06f75d72c533a454fb7b43524b55b9
vrupeshkumar <vrupeshkumar@gmail.com>
2012-10-21 13:47:12 -0400

    Honor Rolls

---
+++ Highlands/StaticModel/OverallGPA.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highlands.StaticModel
+{
+    public class OverallGPA
+    {        
+        public double ComputeGPA()
+        {            
+            //bool bForgiven = false;
+            int nCourseCount = 0;            
+            double gpa = 0.0;   
+                     
+            gpa = gpa + GradePoints("A");
+            nCourseCount++;           
+            
+            gpa = (nCourseCount > 0) ? gpa / nCourseCount : 0.0;
+            
+            return gpa;
+        }
+
+        public static double GradePoints(string grade)
+        {
+            switch (grade)
+            {
+                case "A+":
+                    return 4.3;
+                case "A":
+                    return 4.0;                    
+                case "A-":
+                    return 3.6;
+
+                case "B+":
+                    return 3.3;
+                case "B":
+                    return 3.0;
+                    
+                case "B-":
+                    return 2.6;
+                    
+                case "C+":
+                    return 2.3;
+                    
+                case "C":
+                    return 2.0;
+                    
+                case "C-":
+                    return 1.6;
+                    
+                default:
+                    return 0.0;
+                    
+            }
+        }
+    }
+}
---
+++ Highlands/View/HonorRollsControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Highlands.ViewModel;
+
+namespace Highlands
+{
+    /// <summary>
+    /// Interaction logic for HonorRollsControl.xaml
+    /// </summary>
+    public partial class HonorRollsControl : UserControl
+    {
+        private HonorRollsViewModel model;
+
+        public HonorRollsControl()
+        {
+            InitializeComponent();
+            model = new HonorRollsViewModel();
+            this.DataContext = model;
+        }
+    }
+}
